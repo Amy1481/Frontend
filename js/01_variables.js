276c434a1c145812eb18f57ecae3d13ef599dd4f
Amy1481 <amishamohanty1481@gmail.com>
2025-04-02 23:41:11 +0530

    Add files via upload

---
+++ js/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=13245
+let accountEmail="amsisnd12@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountstate;
+
+//  accountId=2  
+
+ /*
+Prefer not to use car
+because it is not block scopedand functional scoped
+ */
+
+
+accountEmail="hsdgja@gmail.com"
+accountPassword="12356"
+accountCity="Bbsr"
+
+ console.log(accountId); 
+ console.table([accountEmail,accountPassword,accountCity,accountstate]);
