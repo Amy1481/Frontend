353592bc500ce40879a45f45aacc9e4a4621630b
Amisha mohanty <amishamohanty2025@gmail.com>
2025-10-28 23:52:29 +0530

    Add files via upload

---
+++ React/01Vite_React/src/App.jsx
@@ -0,0 +1,14 @@
+import Cafe from "./cafe"
+
+function App() {
+
+  return (
+    <>
+    <Cafe />
+    <h1>Cafe & Garden</h1>
+    <p>Test para</p>
+    </>
+  )
+}
+
+export default App
---
+++ React/01Vite_React/src/cafe.jsx
@@ -0,0 +1,7 @@
+function Cafe(){
+    return(
+        <h3>Cafe is ready!</h3>
+    )
+}
+
+export default Cafe
